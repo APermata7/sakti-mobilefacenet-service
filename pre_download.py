55d145aa8b317470b9a2a0602620f7ac2cbcf7fa
APermata7 <auliapermatakumala@gmail.com>
2026-07-26 20:42:02 +0700

    fix: pakai Nixpacks tanpa package custom

--- pre_download.py
+++
@@ -1,7 +0,0 @@
-import insightface
-import os
-
-print("Downloading buffalo_l model...")
-os.makedirs("models_data", exist_ok=True)
-insightface.app.FaceAnalysis(name='buffalo_l')
-print("Model downloaded successfully")
